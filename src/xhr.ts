ec18109b2103197eede24790a527043812f908a9
ohmyray <ohmyray@163.com>
2020-06-16 17:48:53 +0800

    实现xhr基本步骤

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,5 +1,8 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-  
+  const { data = null, url, method = 'get' } = config // 拿到传入的配置与数据
+  const request = new XMLHttpRequest() // 获取请求对象
+  request.open(method.toUpperCase(), url, true) // 统一方法名的大小写，传入url
+  request.send(data) // 发送数据
 }
